7d9824e47b5b3a8ee5a21f0f19e8dd09fb124f76
Chandra1714 <chandrakanthhv9964@gmail.com>
2025-09-09 11:45:19 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Form from './Form'
-import Test from '/Test'
+import Form from './Form';
+import New from './new';
 
 function App() {
   
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
      <div>
-     <Test/>
+     <New/>
      <Form/>  
      </div>
       
---
+++ frontend/src/New.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const New = () => {
+  return (
+    <div>
+        <h2>Upload Images</h2>
+    </div>
+  )
+}
+
+export default New
