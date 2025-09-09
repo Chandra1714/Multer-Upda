4645664d6580b40948007fd57a9c8bf715e1c6cb
Chandra1714 <chandrakanthhv9964@gmail.com>
2025-09-09 11:50:59 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",          
-    "multer-upda-ia7u.vercel.app" 
+    "multer-upda-qdfh.vercel.app" 
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders:["Content-Type","Authorization"],
